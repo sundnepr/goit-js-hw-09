c63133099ad72c83f065d6104acee8ce6574600a
victor <157064298+sundnepr@users.noreply.github.com>
2024-06-20 14:47:02 +0300

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -2,33 +2,30 @@ import { images } from '/js/gallare-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const divgalEl = document.querySelector('.gallery');
-const newStructEl = images.map((elem) => `
-<div class="gallery__item">
-  <a class="gallery__link" href="${elem.original}">
-    <img class="gallery__image" src="${elem.preview}"
-      data-source="${elem.original}" alt="${elem.description}" />
-  </a>
-</div>`).join("");
-divgalEl.insertAdjacentHTML("beforeend", newStructEl);
-divgalEl.addEventListener("click", selectPicture);
+const containerGallery = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItems(images);
 
-function selectPicture(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") { return };
-  const instance = basicLightbox.create(`
- <img width="1400" height="900" src="${event.target.dataset.source}">`);
-  instance.show(() => { document.addEventListener("keyup", ev => {
-      if (ev.key === 'Escape') { instance.close() }
-    });
-  })
+containerGallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItems(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__tem">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+       </a>
+      </li>
+    `;
+    })
+    .join('');
+}
 
 const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
   captionDelay: 250,
+  captionsData: 'alt',
 });
-};
-
-
-
- 
